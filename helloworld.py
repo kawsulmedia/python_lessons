cfb2e2ab8fc4638e64fc0188f5564bc93a1b8c6f
kirk nelson <Kirk@kawsul.com>
2016-09-10 12:15:57 -0500

    added helloworld python file

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+print ("Hello world!")
+print ("Hello again")
+print ("I like writing this")
+print ("this is fun!")
+print ("Yay! Printing")
+print ("I'd much rather you 'not'.")
+print ('I "said" do not touch this.')
+
+print ("So far I have been printing strings in a single line.")
+
+print ("However python has ways\n to print on multiple lines.\n This is one way to do it")
+print ('''And this is the otheer way I learnt to make multi lined statements''') 
+
+list = ["Hello world", "Hello again", "I like writing this", "This is fun."]
+#print (list [0])
+#print (list [0:3])
+
+for item in list[0:3]:
+	print (item)
